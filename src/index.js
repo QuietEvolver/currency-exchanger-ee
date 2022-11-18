862ce653b89ec8b04bd3a0d9a62c6ecf65ea0523
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 14:47:51 -0800

    It will capture input.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import CurrencyService from './service/currency-service.js';
 
 function getCurrency(currency){
   let promise = CurrencyService.getCurrency(currency);
+  console.log("Promise get currency: ", currency);
 
   promise.then(function (data){
+    console.log("getC res data: ", data)
     printElements(data);
   }, function(error) {
     printError(error);
   });
 }
 
-function printElements(response){
+function printElements(response, currency){
   try {
     // console.log("response: ", response);
     // const baseCode = response.base_code;
-    const conversionRates = response.conversion_rates;
+    const conversionRates = response.conversion_rates; // aka Object.values(c)[7]
+    let dollars = Object.values(response)[8].USD;
+    let USD = currency * dollars; 
+    console.log("dollars: ", dollars);
+    console.log("USD: ", USD);
     // const USD = response.base_code("USD");
     // console.log("USD", USD);
     
@@ -24,7 +30,7 @@ function printElements(response){
     //   console.log("I am in conversion USD", baseCode, conversionRates);
     //   return conversionRates;
     // }
-    document.querySelector("#showResponse").innerText = `Res: ${response} with base code: ${response.base_code} and rates are ${response.conversionRates}`;
+    document.querySelector("#showResponse").innerText = `Res: rates are for your $ ${dollars} in USD`;// ${response} with base code: ${response.base_code} and
     return Object.values(Object.keys(conversionRates));
     // my base code = USD
     // if User wants X, 
@@ -53,10 +59,16 @@ function handleFormSubmit(e) {
   e.preventDefault();
 
   document.getElementById("showResponse").innerText = "";
-  const currency = document.getElementById("currency").value;
-  document.getElementById("currency").value = null;
-  currency === "usd";
-  getCurrency(currency);
+  const currencyUSD = document.getElementById("currencyUSD").value;
+  console.log("currency USD input: ", currencyUSD)
+  document.getElementById("currencyUSD").value = null;
+  
+  getCurrency(currencyUSD);
+  let currency = currencyUSD;
+  console.log("currency e: ", currency);
+  // console.log("printElements currencyUSD: ", printElements(currency));
+  // console.log("currencyUSD input: ", getCurrency(currencyUSD));
+  // console.log("currency USD input: ", getCurrency(currencyUSD));
   // const currency = document.getElementById("currency").value;
   // document.getElementById("currency").value = null;
   // getCurrency(currency);
