10e3e4d65aa554b5e6cb0b0911a7312cb50e1f1e
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 12:48:42 -0800

    It will add currency.

--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -1,16 +1,16 @@
 export default class CurrencyService{
-  static getCurrency(){//currency// {${currency}}`
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/usd`)
-    .then(function(response){
-      if(!response.success){
-        const errorMessage = `${response}`;
-        throw new Error(errorMessage);
-      } else {
-        return response.json();
-      }
-    })
-    .catch(function (error) {
-      return error;
-    })
+  static getCurrency(currency){////usd
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+      .then(function(response){
+        if(!response.success){
+          const errorMessage = `${response}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
