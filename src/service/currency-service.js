d0f3d987a774c59739c6866aa65b00adb5270149
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 13:43:19 -0800

    It will keep fetching obj Obj.

--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -1,15 +1,18 @@
 export default class CurrencyService{
-  static getCurrency(currency){////usd
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+  static getCurrency(){////currency//${currency}`
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/usd`)
       .then(function(response){
-        if(!response.success){
+        if(!response.ok){
           const errorMessage = `${response}`;
+          console.log("respone: ", response);
           throw new Error(errorMessage);
         } else {
+          console.log("respone: ", response);
           return response.json();
         }
       })
       .catch(function (error) {
+        console.log("err api: ", error);
         return error;
       });
   }
