5306e434da382072447755f05f1067b48682dd3f
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 11:52:43 -0800

    It will add error handling.

--- src/index.js
+++ src/index.js
@@ -14,9 +14,18 @@ function getCurrency(currency){
 
 function printElements(response){
   try {
+    console.log("response: ", response);
     const baseCode = response.base_code;
     const conversionRates = response.conversion_rates;
+    const USD = response.base_code("USD");
+    console.log("USD", USD);
+    
+    if(baseCode === USD){
+      console.log("I am in conversion USD", baseCode, conversionRates);
+      return conversionRates;
+    }
 
+    return Object.values(Object.keys(conversionRates));
     // my base code = USD
     // if User wants X, 
     // then { conversionRates will return USD in X format } 
@@ -31,7 +40,7 @@ function printElements(response){
     // if User wants B, 
     // then { conversionRates will return USD in B format } 
   } catch (error) {
-    document.querySelector("p#showResponse").innerText += "This is not available. Check back later."
+    document.querySelector("p#showResponse").innerText += "This is not available. Check back later.";
   }
 }
 
@@ -47,10 +56,13 @@ function handleFormSubmit(e) {
   const currency = document.getElementById("currency").value;
   document.getElementById("currency").value = null;
   getCurrency(currency);
+  // const currency = document.getElementById("currency").value;
+  // document.getElementById("currency").value = null;
+  // getCurrency(currency);
 }
 
 window.addEventListener("load", function(){
   this.document.querySelector("form").addEventListener("submit", handleFormSubmit);
-})
+});
 
 
