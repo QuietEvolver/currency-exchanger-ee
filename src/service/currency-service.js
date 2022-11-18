b6ffe063d9a1a598bf9d2c26ce4ee7e1951c8220
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 09:14:46 -0800

    It will add success res to currency service class.

--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -2,7 +2,7 @@ export default class CurrencyService{
   static getCurrency(currency){
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
     .then(function(response){
-      if(!response.ok){
+      if(!response.success){
         const errorMessage = `${response}`;
         throw new Error(errorMessage);
       } else {
