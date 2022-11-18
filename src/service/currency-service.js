043e14f078822f9d17d07ec770a3b72938eb859d
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-18 10:26:23 -0800

    It will update call.

--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService{
-  static getCurrency(currency){
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static getCurrency(){//currency// {${currency}}`
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/usd`)
     .then(function(response){
       if(!response.success){
         const errorMessage = `${response}`;
