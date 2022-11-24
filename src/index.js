83119a42be3430c138b1ae7218100ef55d0c325a
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-23 17:26:02 -0800

    It will add euro conversion.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function getCurrency(currency){
     let currencyIntake = currency * dollars; 
     // console.log(" get currency dollars: ", dollars); // $1 
     // console.log(" get currency USD intake calc: ", currencyIntake); // $20
-    // document.querySelector("#showResponse").innerText = `Res in promise call: rates are for your $ ${USD} in USD \n`;
+    document.querySelector("#showResponse").innerText = `Res in promise call: rates are for your $ ${currencyIntake} in USD \n`;
 
     printElements(data, currencyIntake);
   }, function(error) {
@@ -20,56 +20,55 @@ function getCurrency(currency){
 }
 
 function printElements(data, currencyIntake){
-
-  // let dollars = currency * dollar;
-  // console.log("TprintElements ßcurrency: ", dollars);
-  console.log("TprintElements currency: ", currencyIntake);
-  // let currency = data.currency ;
- 
+  try {  
     let dollar = Object.values(data)[8].USD;
-  try { 
-    if(currencyIntake > 0){
-
-      console.log("TprintElements dollar: ", dollar);
-      
-      console.log("If STMT ßcurrency: ", currencyIntake);
-      return currencyIntake;
-  }
-    // console.log("TryCatch response: ", response);
+    // RESPONSE.CONVERSION_RATE x usdCurrencyDOMIntake = rate;
+    let dollars = Object.values(data)[8].USD;
     console.log("TryCatch data: ", data);
     console.log("TryCatch ßcurrency: ", dollar);
     // const baseCode = response.base_code;
     // const conversionRates = response.conversion_rates; // aka Object.values(response)[7]
-    
-    // RESPONSE.CONVERSION_RATE x usdCurrencyDOMIntake = rate;
-    let dollars = Object.values(data)[8].USD;
-    let euros = Object.values(data)[8].EUR;  // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/EUR
+    let euros = currencyIntake * Object.values(data)[8].EUR;  // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/EUR
       // currencyIntake *= euros;
-    let roubles = Object.values(data)[8].RUB; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/RUB
-    let cordobas = Object.values(data)[8].NIO; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/NIO
-    let nairas = Object.values(data)[8].NGN; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/NGN
-    let pounds = Object.values(data)[8].GBP; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/GBP 
+      console.log("euros: ", euros);
+    let roubles = currencyIntake * Object.values(data)[8].RUB; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/RUB
+    let cordobas = currencyIntake * Object.values(data)[8].NIO; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/NIO
+    let nairas = currencyIntake * Object.values(data)[8].NGN; // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/NGN
+    let pounds = currencyIntake * Object.values(data)[8].GBP; 
+    if(currencyIntake > 0){
+      console.log("TprintElements dollar: ", dollar);      
+      console.log("If STMT ßcurrency: ", currencyIntake);
+
+  // https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/GBP 
     // let USD = currencyUSD * dollars; 
     console.log("dollars: ", dollars);
-    // console.log("currencyUSD: ", currencyUSD);
-    // console.log("USD: ", USD);
-    // const USD = response.base_code("USD");
-    // console.log("USD", USD);
-    
-    // if(baseCode === USD){
-    //   console.log("I am in conversion USD", baseCode, conversionRates);
-    //   return conversionRates;
-    // }
+    return currencyIntake;
+  }
+
+  /*Order.prototype.calculateOrderPrice = function(){
+  if(this.toppings.price === 0){
+    return this.size;
+  }
+  else if(this.size === "small"){
+    this.price = 5 + (this.toppings.length *2);
+    return this.price;
+  } else if(this.size === "medium"){
+    this.price = 10 + (this.toppings.length *2);
+    return this.price;
+  } else if(this.size === "large"){
+    this.price = 15 + (this.toppings.length *2);
+    return this.price;
+  }
+} */
+
     document.querySelector("#showResponse").innerText = `Res: rates are for your USD $ ${currencyIntake} \n
-    for every USD $ ${dollars} \n
+    Currently, for every USD $ ${dollars} \n
     Euro(s) € ${euros} \n 
     Rouble(s) ₽ ${roubles} \n 
     Cordoba(s) C$ ${cordobas} \n 
     Naira(s) ₦ ${nairas} \n 
     Pound(s) £ ${pounds} \n `;
-    // ${response} with base code: ${response.base_code} and
-    // return Object.values(Object.keys(conversionRates));
-    // my base code = USD
+  
     // if User wants X, 
     // then { conversionRates will return USD in X format } 
     // if User wants Y, 
